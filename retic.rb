af40b8e139e8dca0f7fc39493a4c29e3649e57ed
Tatsuki Sugiura <sugi@nemui.org>
2012-05-25 15:07:39 +0900

    URL generation, redirect, forward, layout support.

--- retic.rb
+++ retic.rb
@@ -9,6 +9,16 @@ require 'cgi'
 require 'erb'
 
 class Retic
+  class AbortAction < Exception
+    def initialize(new_action, params = {})
+      @new_action = new_action
+      @params = params
+    end
+    attr_accessor :new_action, :params
+  end
+  class RedirectAction < AbortAction; end
+  class ForwardAction < AbortAction; end
+
   VERSION = "0.1.0"
 
   # Options:
@@ -21,20 +31,43 @@ class Retic
     @cur_action = nil
     @cur_template = nil
     @cgi_headers = opts[:cgi_headers] || {"Cache-Control" => "no-cache"}
+    @action_key = 'action'
   end
   attr_accessor :cgi, :templatedir, :charset, :cgi_headers
 
   # action runner.
   def run
-    @cur_template = @cur_action ||= param('action') || 'index'
-    unless respond_to? "do_#{@cur_action}"
-      cgi.out(gen_cgi_headers("type" => "text/plain", "status" => "400")) {
-	"No action '#{CGI.escapeHTML(@cur_action)}'."
-      }
-      return
+    pre_execute
+    forward_count = 0
+    begin
+      @cur_template = @cur_action ||= param(@action_key) || 'index'
+      unless respond_to? "do_#{@cur_action}"
+        cgi.out(gen_cgi_headers("type" => "text/plain", "status" => "400")) {
+          "No action '#{CGI.escapeHTML(@cur_action)}'."
+        }
+        return
+      end
+      args = __send__("do_#{@cur_action}") || {}
+      @cur_template and render @cur_template, args
+    rescue ForwardAction => e
+      forward_count += 1
+      forward_count > 20 and
+        raise "Forward count exceeded. Forward may loop."
+      @cur_action = e.new_action
+      e.params.each do |k, v|
+        @cgi[k.to_s] = [v]
+      end
+      retry
+    rescue RedirectAction => e
+      raw_redirect url(e.new_action, e.params)
     end
-    args = __send__("do_#{@cur_action}") || {}
-    @cur_template and render @cur_template, args
+    post_execute
+  end
+
+  def pre_execute
+  end
+
+  def post_execute
   end
 
   def gen_cgi_headers(opts = {})
@@ -68,11 +101,32 @@ class Retic
     vars = default_vars
     view.set_variable(vars.merge(user_vars))
     cgi.out(gen_cgi_headers) {
-      view.include_template name
+      if File.exists? "#{templatedir}/layout.erb.html"
+        content = view.include_template name
+        view.set_variable :content => content
+        view.include_template "layout"
+      else
+        view.include_template name
+      end
     }
   end
 
-  def redirect(url)
+  def url(action, query_params = {})
+    query = query_params.merge(@action_key => action).map {|k, v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join(';')
+    ret = self_url
+    query and ret += "?#{query}"
+    ret
+  end
+
+  def forward(action, params = {})
+    raise ForwardAction.new(action, params)
+  end
+
+  def redirect(action, params = {})
+    raise RedirectAction.new(action, params)
+  end
+
+  def raw_redirect(url)
     @cur_template = nil
     cgi.print cgi.header({"Location" => url})
   end
